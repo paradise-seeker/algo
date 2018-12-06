10ef76123ea4c61d889dd01f5b77aa85cb38645c
paradise-seeker <run_hxl@163.com>
2018-12-06 22:34:56 +0800

    Update 01_MySorts.java

--- Sorts/01_MySorts.java
+++ Sorts/01_MySorts.java
@@ -26,9 +26,8 @@ class MySorts{
 	}
 	//插入排序，a表示数组，n表示数组大小
 	public static void insertionSort(int[] a, int n) {
-		if (n <= 1)
-			return;
-
+		if (n <= 1)  return;
+		
 		for (int i = 1; i < n; ++i) {
 			int value = a[i];
 			int j = i - 1;
